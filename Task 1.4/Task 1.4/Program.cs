badc1b0eee3ba4eec430a6362a5ce9c7b17ed412
Michael-200 <55751246+Michael-200@users.noreply.github.com>
2020-03-16 19:13:05 +0300

    Add Task 1.4

---
+++ Task 1.4/Task 1.4/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Task_1._4
+{
+    internal class Program
+    {
+        const int N = 13;
+
+        static int[,] FillingOut(int line, int column)
+        {
+            int size = line * column;
+            int[,] array = new int[line, column];
+
+            int horizontally = 0;
+            int upright = 0;
+            int maxColumn = N - 1;
+            int minColumn = 0;
+            int maxRow = N - 1;
+            int minRow = 0;
+
+
+            while (size > 1)
+            {
+                //Движемся вправо.
+                while (upright <= maxColumn)
+                {
+                    array[horizontally, upright] = size;
+                    size--;
+                    upright++;
+                }
+                ++minRow;
+                --upright;
+                ++horizontally;
+
+                //Движемся вниз.
+                while (horizontally <= maxRow)
+                {
+                    array[horizontally, upright] = size;
+                    size--;
+                    horizontally++;
+                }
+                --maxColumn;
+                --horizontally;
+                --upright;
+
+
+                //Движемся влево.
+                while (upright >= minColumn)
+                {
+                    array[horizontally, upright] = size;
+                    size--;
+                    upright--;
+                }
+                --maxRow;
+                ++upright;
+                --horizontally;
+
+                //Движемся вверх.
+                while (horizontally >= minRow)
+                {
+                    array[horizontally, upright] = size;
+                    size--;
+                    horizontally--;
+                }
+                ++minColumn;
+                ++horizontally;
+                ++upright;
+            }
+            array[N / 2, N / 2] = 1;
+            return array;
+        }
+        static void Main(string[] args)
+        {
+            int line = N;
+            int column = N;
+            int[,] array = Program.FillingOut(line, column);
+
+            //Выводим массив в консоль.
+            for (int firstHorizontally = 0; firstHorizontally < line; firstHorizontally++)
+            {
+                for (int firstUpright = 0; firstUpright < column; firstUpright++)
+                {
+                    Console.Write(array[firstHorizontally, firstUpright] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
