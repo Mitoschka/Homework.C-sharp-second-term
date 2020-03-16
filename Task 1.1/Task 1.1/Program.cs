c913f5fd22e7bb0a2e85cbbc75691676ba074af7
Michael-200 <55751246+Michael-200@users.noreply.github.com>
2020-03-16 19:05:56 +0300

    Add Task 1.1

---
+++ Task 1.1/Task 1.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Задача_1._1
+{
+    class Program
+    {
+        private static int Factorial(int number)
+            => number <= 1 ? 1 : number * Factorial(number - 1);
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Factorial(5) = {Factorial(5)}");
+            Console.WriteLine($"Factorial(-4) = {Factorial(-4)}");
+            Console.WriteLine($"Factorial(23) = {Factorial(23)}");
+        }
+    }
+}
