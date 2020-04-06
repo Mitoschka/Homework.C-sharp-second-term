ae24d112b44a8024828a54e1084bc5fc74acec3e
Michael-200 <55751246+Michael-200@users.noreply.github.com>
2020-04-06 16:50:48 +0300

    Add Task 7.2

---
+++ Task 7.2/Analog Clock/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Analog_Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.backBox = new System.Windows.Forms.PictureBox();
+            this.hourBox = new System.Windows.Forms.PictureBox();
+            this.minuteBox = new System.Windows.Forms.PictureBox();
+            this.dotBox = new System.Windows.Forms.PictureBox();
+            this.secondBox = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblDay = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.backBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hourBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minuteBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dotBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secondBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // backBox
+            // 
+            this.backBox.BackColor = System.Drawing.Color.Transparent;
+            this.backBox.Location = new System.Drawing.Point(37, 14);
+            this.backBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.backBox.Name = "backBox";
+            this.backBox.Size = new System.Drawing.Size(450, 462);
+            this.backBox.TabIndex = 0;
+            this.backBox.TabStop = false;
+            // 
+            // hourBox
+            // 
+            this.hourBox.BackColor = System.Drawing.Color.Transparent;
+            this.hourBox.Location = new System.Drawing.Point(74, 69);
+            this.hourBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.hourBox.Name = "hourBox";
+            this.hourBox.Size = new System.Drawing.Size(450, 462);
+            this.hourBox.TabIndex = 1;
+            this.hourBox.TabStop = false;
+            // 
+            // minuteBox
+            // 
+            this.minuteBox.BackColor = System.Drawing.Color.Transparent;
+            this.minuteBox.Location = new System.Drawing.Point(176, 108);
+            this.minuteBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.minuteBox.Name = "minuteBox";
+            this.minuteBox.Size = new System.Drawing.Size(450, 462);
+            this.minuteBox.TabIndex = 2;
+            this.minuteBox.TabStop = false;
+            // 
+            // dotBox
+            // 
+            this.dotBox.BackColor = System.Drawing.Color.Transparent;
+            this.dotBox.Location = new System.Drawing.Point(256, 153);
+            this.dotBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dotBox.Name = "dotBox";
+            this.dotBox.Size = new System.Drawing.Size(450, 462);
+            this.dotBox.TabIndex = 3;
+            this.dotBox.TabStop = false;
+            this.dotBox.Click += new System.EventHandler(this.dotBox_Click);
+            // 
+            // secondBox
+            // 
+            this.secondBox.BackColor = System.Drawing.Color.Transparent;
+            this.secondBox.Location = new System.Drawing.Point(436, 192);
+            this.secondBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.secondBox.Name = "secondBox";
+            this.secondBox.Size = new System.Drawing.Size(450, 462);
+            this.secondBox.TabIndex = 4;
+            this.secondBox.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.BackColor = System.Drawing.Color.Transparent;
+            this.lblTime.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(204, 502);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(0, 31);
+            this.lblTime.TabIndex = 5;
+            this.lblTime.Click += new System.EventHandler(this.lblTime_Click);
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.BackColor = System.Drawing.Color.Transparent;
+            this.lblDate.Font = new System.Drawing.Font("Segoe Print", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.Location = new System.Drawing.Point(31, 465);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(0, 35);
+            this.lblDate.TabIndex = 7;
+            // 
+            // lblDay
+            // 
+            this.lblDay.AutoSize = true;
+            this.lblDay.BackColor = System.Drawing.Color.Transparent;
+            this.lblDay.Font = new System.Drawing.Font("Segoe Print", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblDay.Location = new System.Drawing.Point(344, 465);
+            this.lblDay.Name = "lblDay";
+            this.lblDay.Size = new System.Drawing.Size(0, 35);
+            this.lblDay.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 559);
+            this.Controls.Add(this.lblDay);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.secondBox);
+            this.Controls.Add(this.dotBox);
+            this.Controls.Add(this.minuteBox);
+            this.Controls.Add(this.hourBox);
+            this.Controls.Add(this.backBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Часы";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.backBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hourBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minuteBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dotBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secondBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox backBox;
+        private System.Windows.Forms.PictureBox hourBox;
+        private System.Windows.Forms.PictureBox minuteBox;
+        private System.Windows.Forms.PictureBox dotBox;
+        private System.Windows.Forms.PictureBox secondBox;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblDay;
+    }
+}
+
---
+++ Task 7.2/Analog Clock/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Analog_Clock
+{
+    public partial class Form1 : Form
+    {
+        Bitmap back, hour, minute, dot, second;
+
+        private void dotBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTime_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblSecond_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblTime.Text = DateTime.Now.ToString("HH:mm:ss");
+            lblDate.Text = DateTime.Now.ToString("MMM dd yyyy");
+            lblDay.Text = DateTime.Now.ToString("dddd");
+
+            DateTime Now = DateTime.Now;
+            int Hour = Now.Hour;
+            int Minute = Now.Minute;
+            int Second = Now.Second;
+
+            Single AngleS = Second * 6;
+            Single AngleM = Minute * 6 + AngleS / 60;
+            Single AngleH = Hour * 30 + AngleM / 12;
+
+            backBox.Image = back;
+
+            backBox.Controls.Add(hourBox);
+            hourBox.Location = new Point(0, 0);
+            hourBox.Image = rotateImage(hour, AngleH);
+
+
+            hourBox.Controls.Add(minuteBox);
+            minuteBox.Location = new Point(0, 0);
+            minuteBox.Image = rotateImage(minute, AngleM);
+
+            minuteBox.Controls.Add(dotBox);
+            dotBox.Location = new Point(0, 0);
+            dotBox.Image = dot;
+
+
+            dotBox.Controls.Add(secondBox);
+            secondBox.Location = new Point(0, 0);
+            secondBox.Image = rotateImage(second, AngleS);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            back = new Bitmap(".\\back.png");
+            hour = new Bitmap(".\\hour.png");
+            minute = new Bitmap(".\\minute.png");
+            dot = new Bitmap(".\\dot.png");
+            second = new Bitmap(".\\second.png");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private Bitmap rotateImage(Bitmap rotateMe, float angle)
+        {
+            Bitmap rotatedImage = new Bitmap(rotateMe.Width, rotateMe.Height);
+
+            using(Graphics g = Graphics.FromImage(rotatedImage))
+            {
+                g.TranslateTransform(rotateMe.Width / 2, rotateMe.Height / 2);
+                g.RotateTransform(angle);
+                g.TranslateTransform(-rotateMe.Width / 2, -rotateMe.Height / 2);
+                g.DrawImage(rotateMe, new Point(0, 0));
+            }
+
+            return rotatedImage;
+        }
+    }
+}
