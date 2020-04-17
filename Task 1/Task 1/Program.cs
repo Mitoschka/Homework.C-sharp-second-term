7a57b2e35b992fa2f7e648db411d2a62aec08dc4
Michael-200 <55751246+Michael-200@users.noreply.github.com>
2020-04-17 16:19:37 +0300

    Add Task 1

---
+++ Task 1/Task 1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> numbers = new Queue<int>();
+
+            numbers.Enqueue(3); // очередь 3
+            numbers.Enqueue(5); // очередь 3, 5
+            numbers.Enqueue(8); // очередь 3, 5, 8
+
+            // получаем первый элемент очереди
+            int queueElement = numbers.Dequeue(); //теперь очередь 5, 8
+            Console.WriteLine(queueElement);
+
+            Queue<Person> persons = new Queue<Person>();
+            persons.Enqueue(new Person() { Name = "Tom" });
+            persons.Enqueue(new Person() { Name = "Bill" });
+            persons.Enqueue(new Person() { Name = "John" });
+
+            // получаем первый элемент без его извлечения
+            Person pp = persons.Peek();
+            Console.WriteLine(pp.Name);
+
+            Console.WriteLine("Сейчас в очереди {0} человек", persons.Count);
+
+            // теперь в очереди Tom, Bill, John
+            foreach (Person p in persons)
+            {
+                Console.WriteLine(p.Name);
+            }
+
+            // Извлекаем первый элемент в очереди - Tom
+            Person person = persons.Dequeue(); // теперь в очереди Bill, John
+            Console.WriteLine(person.Name);
+
+            Console.ReadLine();
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+    }
+}
