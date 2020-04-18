0f88e50eaf04f81e396842c278df7720e56566c9
Michael-200 <55751246+Michael-200@users.noreply.github.com>
2020-04-18 16:40:11 +0300

    Add new Task

---
+++ Task 7.1/Task 7.1.Test/Task 7.1.Test.cs
@@ -0,0 +1,102 @@
+using NUnit.Framework;
+
+namespace Task_7._1.Test
+{
+    public class Tests
+    {
+        CalculatorLogic calculatorLogic;
+
+        [SetUp]
+        public void Setup()
+        {
+            calculatorLogic = new CalculatorLogic();
+        }
+
+        [Test]
+        public void TestOfCorrectWorkOfPlus()
+        {
+            calculatorLogic.Sumbol = '+';
+            calculatorLogic.FirstNum = 5;
+            calculatorLogic.SecondNum = 6;
+
+            calculatorLogic.Operation();
+
+            Assert.AreEqual(11, calculatorLogic.FirstNum);
+        }
+        [Test]
+        public void TestOfCorrectWorkOfMinus()
+        {
+            calculatorLogic.Sumbol = '-';
+            calculatorLogic.FirstNum = -30;
+            calculatorLogic.SecondNum = -4;
+
+            calculatorLogic.Operation();
+
+            Assert.AreEqual(-26, calculatorLogic.FirstNum);
+        }
+        [Test]
+        public void TestOfCorrectWorkOfDivised()
+        {
+            calculatorLogic.Sumbol = '/';
+            calculatorLogic.FirstNum = 15;
+            calculatorLogic.SecondNum = 3;
+
+            calculatorLogic.Operation();
+
+            Assert.AreEqual(5, calculatorLogic.FirstNum);
+        }
+        [Test]
+        public void TestOfCorrectWorkOfMultiply()
+        {
+            calculatorLogic.Sumbol = '*';
+            calculatorLogic.FirstNum = 5;
+            calculatorLogic.SecondNum = -3;
+
+            calculatorLogic.Operation();
+
+            Assert.AreEqual(-15, calculatorLogic.FirstNum);
+        }
+        [Test]
+        public void TestOfCorrectWorkOfSqrt()
+        {
+            double resultOfSqrt = 0;
+            calculatorLogic.FirstNum = 25;
+
+            calculatorLogic.SqrtMath(resultOfSqrt);
+
+            Assert.AreEqual(5, calculatorLogic.FirstNum);
+        }
+        [Test]
+        public void TestOfCorrectWorkOfDivisedOnOne()
+        {
+            double resultOfDivised = 0;
+            calculatorLogic.FirstNum = 5;
+
+            calculatorLogic.DivisedOnOne(resultOfDivised);
+
+            Assert.AreEqual(0.20, calculatorLogic.FirstNum);
+        }
+        [Test]
+        public void TestOfCorrectWorkOfPercent()
+        {
+            calculatorLogic.Sumbol = '%';
+            calculatorLogic.FirstNum = 10;
+            calculatorLogic.SecondNum = 100;
+
+            calculatorLogic.Operation();
+
+            Assert.AreEqual(10, calculatorLogic.FirstNum);
+        }
+        [Test]
+        public void TestOfCorrectWorkOfRemove()
+        {
+            calculatorLogic.FirstNum = 5;
+            calculatorLogic.SecondNum = 6;
+
+            calculatorLogic.Remove();
+
+            Assert.AreEqual(0, calculatorLogic.FirstNum);
+            Assert.AreEqual(0, calculatorLogic.SecondNum);
+        }
+    }
+}
---
+++ Task 7.1/Task 7.1/CalculatorLogic.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_7._1
+{
+    public class CalculatorLogic
+    {
+        double firstNum = 0;
+        double secondNum = 0;
+        double result = 0;
+        char sumbol = ' ';
+        bool isOperationPressedEarly = false;
+        bool isEqualPressed = false;
+        bool isContainDot = false;
+
+        public bool IsOperationPressedEarly { get => isOperationPressedEarly; set => isOperationPressedEarly = value; }
+        public bool IsContainDot { get => isContainDot; set => isContainDot = value; }
+        public bool IsEqualPressed { get => isEqualPressed; set => isEqualPressed = value; }
+        public double SecondNum { get => secondNum; set => secondNum = value; }
+        public double FirstNum { get => firstNum; set => firstNum = value; }
+        public char Sumbol { get => sumbol; set => sumbol = value; }
+
+        internal void EqualButtonPressed()
+        {
+            Operation();
+            IsOperationPressedEarly = false;
+            IsEqualPressed = true;
+        }
+        public void Operation()
+        {
+            switch (Sumbol)
+            {
+                case '+':
+                    {
+
+                        result = FirstNum + SecondNum;
+                        FirstNum = result;
+                        break;
+                    }
+                case '-':
+                    {
+                        result = FirstNum - SecondNum;
+                        FirstNum = result;
+                        break;
+                    }
+                case '*':
+                    {
+                        result = FirstNum * SecondNum;
+                        FirstNum = result;
+                        break;
+                    }
+                case '/':
+                    {
+                        result = FirstNum / SecondNum;
+                        FirstNum = result;
+                        break;
+                    }
+                case '%':
+                    {
+                        result = FirstNum * SecondNum / 100;
+                        FirstNum = result;
+                        break;
+                    }
+            }
+        }
+        public void Remove()
+        {
+            FirstNum = 0;
+            SecondNum = 0;
+        }
+        public void SqrtMath(double resultOfSqrt)
+        {
+            resultOfSqrt = Math.Sqrt(firstNum);
+            firstNum = resultOfSqrt;
+        }
+        public void DivisedOnOne(double resultDivisedOnOne)
+        {
+            resultDivisedOnOne = 1 / firstNum;
+            firstNum = resultDivisedOnOne;
+        }
+    }
+}
