3bedfaed2463a0c1c134481c2fbd64b964a95fdf
Michael-200 <55751246+Michael-200@users.noreply.github.com>
2020-05-08 19:39:36 +0600

    Add Test

---
+++ WindowsFormsApp1/WindowsFormsApp1/CatchUpButton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace RunningButton
+{
+    public partial class CatchUpButton : Form
+    {
+        private Random randomNumber = new Random();
+        public CatchUpButton()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Метод, который меняет расположение кнопки, если ее касается курсор.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CursorTouchesRunawayButton(object sender, MouseEventArgs e)
+        {
+            Button button = sender as Button;
+            int cursorX = Cursor.Position.X;
+            int cursorY = Cursor.Position.Y;
+            int newButtonX = cursorX;
+            int newButtonY = cursorY;
+            while (cursorX >= newButtonX && cursorX <= newButtonX + button.Width && cursorY >= newButtonY && cursorY <= newButtonY + button.Height)
+            {
+                newButtonX = randomNumber.Next(0, this.ClientSize.Width - button.Width);
+                newButtonY = randomNumber.Next(0, this.ClientSize.Height - button.Height);
+            }
+            button.Location = new System.Drawing.Point(newButtonX, newButtonY);
+        }
+
+        /// <summary>
+        /// Метод, который открывает messageBox, если кнопка была нажата.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnRunawayButtonClick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Fail.");
+        }
+    }
+}
