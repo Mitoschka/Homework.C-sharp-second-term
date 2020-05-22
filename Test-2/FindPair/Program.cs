94378d68a90b59c1458ecb1c2edf6bcd1673b3cc
Michael-200 <55751246+Michael-200@users.noreply.github.com>
2020-05-22 21:53:55 +0600

    Deleted unnecessary using

--- Test-2/FindPair/Program.cs
+++ Test-2/FindPair/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/// <summary>
+/// Пространство имён
+/// </summary>
 namespace FindPair
 {
     static class Program
