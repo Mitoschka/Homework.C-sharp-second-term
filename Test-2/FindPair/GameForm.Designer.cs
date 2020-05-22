4d1b21848e2ecff6e9e8aa8b96d75020e41c0fb7
Michael-200 <55751246+Michael-200@users.noreply.github.com>
2020-05-22 21:15:26 +0600

    Fixed Error with wrong components

--- Test-2/FindPair/GameForm.Designer.cs
+++ Test-2/FindPair/GameForm.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent(int sizeOfTable)
         {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.timerForStepOfPlayer = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.BackColor = System.Drawing.Color.PeachPuff;
+            this.tableLayoutPanel.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset;
+            this.tableLayoutPanel.ColumnCount = sizeOfTable;
+            this.tableLayoutPanel.RowCount = sizeOfTable;
+            for (int i = 0; i != sizeOfTable; ++i)
+            {
+                float width = 100 / (sizeOfTable);
+                this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, width));
+                this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, width));
+            }
+
+            for (int i = 0; i != sizeOfTable; ++i)
+            {
+                for (int j = 0; j != sizeOfTable; ++j)
+                {
+                    System.Windows.Forms.Label newLabel = new System.Windows.Forms.Label();
+                    newLabel.BackColor = System.Drawing.Color.PeachPuff;
+                    newLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+                    newLabel.Font = new System.Drawing.Font("Comic Sans MS", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+                    newLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+                    newLabel.Click += new System.EventHandler(this.clickOnLabel);
+                    this.tableLayoutPanel.Controls.Add(newLabel, i, j);
+                }
+            }
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.Size = new System.Drawing.Size(534, 511);
+            this.tableLayoutPanel.TabIndex = 0;
+            // 
+            // timerForStepOfPlayer
+            // 
+            this.timerForStepOfPlayer.Interval = 750;
+            this.timerForStepOfPlayer.Tick += new System.EventHandler(this.tickOfTheTimer);
             // 
             // GameForm
             // 
-            this.ClientSize = new System.Drawing.Size(278, 244);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 511);
+            this.Controls.Add(this.tableLayoutPanel);
             this.Name = "GameForm";
-            this.Load += new System.EventHandler(this.GameForm_Load);
+            this.Text = "Find The Pair!";
+            this.tableLayoutPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
